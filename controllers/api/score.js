dbefa4f73bffce65b632766bdafc6b80ec0969cb
anthonyprograms <anthony.williams@student.csulb.edu>
2015-10-03 09:56:29 -0700

    Score and user API

--- controllers/api/score.js
+++ controllers/api/score.js
@@ -3,19 +3,25 @@ var router = require('express').Router()
 
 router.get('/api/scores', function (req, res, next) {
     Score.find()
-    .sort('-date')
+    .sort('score')
     .exec(function(err, scores) {
         if (err) { return next(err) }
         res.json(scores)
     })
 })
 
+router.put('/api/scores', function (req, res, next) {
+    Score.findOneAndUpdate({username:req.body.username}, {score: req.body.score}, {new:true}, function (err, s) {
+        res.send(201).json(s)
+    })
+})
+
 router.post('/api/scores', function (req, res, next) {
     var score = new Score({points: req.body.score})
     //score.username = req.auth.username
-    score.save(function (err, post) {
+    score.save(function (err, s) {
         if (err) { return next(err) }
-        res.status(201).json(post)
+        res.status(201).json(s)
     })
 })
 
